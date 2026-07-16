ced806ee2550a822a6c34ce906b71fbee188121e
tbouali <taktakbaba03@gmail.com>
2026-07-16 17:03:56 +0200

    mainipulation-DOM

---
+++ SUIVI!_PROJET_ZAKARIA/MANIP-DOM-JS/app.js
@@ -0,0 +1,12 @@
+
+function MARHABA () {
+alert("Hello World, MARHABA")
+}
+
+function affichtex(){
+alert(document.getElementById("aff").value)
+// alert (document.getElementById('aff').value)
+
+
+}
+
